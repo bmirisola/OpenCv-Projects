d432b1e931a6fad5ca863bdebe0174208dd6bf90
Benny Mirisola <bennymirisola@gmail.com>
2019-10-13 14:49:58 -0400

    deleted focus_absolute

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -11,7 +11,7 @@ v4l2-ctl -d /dev/video$deviceId --set-ctrl=exposure_auto=0
 #v4l2-ctl -d /dev/video$deviceId --set-ctrl=exposure_auto_priority=0
 #v4l2-ctl -d /dev/video$deviceId --set-ctrl=exposure_absolute=1
 v4l2-ctl -d /dev/video$deviceId --set-ctrl=focus_auto=1
-v4l2-ctl -d /dev/video$deviceId --set-ctrl=focus_absolute=0
+#v4l2-ctl -d /dev/video$deviceId --set-ctrl=focus_absolute=0
 #v4l2-ctl -d /dev/video$deviceId --set-ctrl=led1_mode=0
 
 
