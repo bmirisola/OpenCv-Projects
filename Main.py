7f013cd20af7a6b67fb6ed33cf4b526c05ae3986
Benny Mirisola <bennymirisola@gmail.com>
2019-10-13 15:31:20 -0400

    added bounding box and circle

--- Main.py
+++ Main.py
@@ -29,6 +29,36 @@ while (True):
 
     hsv = cv2.cvtColor(normalized_frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_range, upper_range)
+    contours, hier = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(c)
+        # draw a green rectangle to visualize the bounding rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # get the min area rect
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+        # convert all coordinates floating point values to int
+        box = np.int0(box)
+        # draw a red 'nghien' rectangle
+        cv2.drawContours(frame, [box], 0, (0, 0, 255))
+
+        # finally, get the min enclosing circle
+        (x, y), radius = cv2.minEnclosingCircle(c)
+        # convert all values to int
+        center = (int(x), int(y))
+        radius = int(radius)
+        # and draw the circle in blue
+        frame = cv2.circle(frame, center, radius, (255, 0, 0), 2)
+
+    print(len(contours))
+    cv2.drawContours(frame, contours, -1, (255, 255, 0), 1)
+
+    cv2.imshow("contours", frame)
+
+    cv2.imshow("contours", frame)
 
     cv2.imshow('hsv', hsv)
     cv2.imshow('frame', frame)
