90e5298295cca1c7dfa03a8fda659942727cdc07
Benny Mirisola <bennymirisola@gmail.com>
2019-10-14 13:19:33 -0400

    replaced img with frame

--- facial_recognition.py
+++ facial_recognition.py
@@ -20,8 +20,8 @@ minW = 0.1 * cap.get(3)
 minH = 0.1 * cap.get(4)
 
 while True:
-    ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
         gray,
@@ -30,7 +30,7 @@ while True:
         minSize=(int(minW), int(minH)),
     )
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         id, confidence = recognizer.predict(gray[y:y + h, x:x + w])
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
@@ -40,10 +40,10 @@ while True:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
 
-        cv2.putText(img, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
-        cv2.putText(img, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
+        cv2.putText(frame, str(id), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+        cv2.putText(frame, str(confidence), (x + 5, y + h - 5), font, 1, (255, 255, 0), 1)
 
-    cv2.imshow('camera', img)
+    cv2.imshow('camera', frame)
     k = cv2.waitKey(10) & 0xff  # Press 'ESC' for exiting video
     if k == 27:
         break
