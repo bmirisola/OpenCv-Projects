341e5a363f04a31dce40270a49c69b5a7f18ad87
Benny Mirisola <bennymirisola@gmail.com>
2019-10-14 13:09:15 -0400

    updated scale_factor, minimum neighbors, and minimum size

--- Constants.py
+++ Constants.py
@@ -1,6 +1,6 @@
 CAPTURE_SOURCE_ID = 0
 HAARCASCADE_LOCATION = '/home/pi/python_projects/OpenCV/data/haarcascades/haarcascade_frontalface_default.xml'
 
-SCALE_FACTOR = 1.05
+SCALE_FACTOR = 1.2
 MINIMUM_NEIGHBORS = 6
 MINIMUM_SIZE = (30,30)
