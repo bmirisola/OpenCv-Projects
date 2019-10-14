42d4822cb581ccf51bfef6b51296ab37d023360b
Benny Mirisola <bennymirisola@gmail.com>
2019-10-13 22:05:10 -0400

    added all of face detection

--- Main.py
+++ Main.py
@@ -2,12 +2,20 @@ import cv2
 import numpy as np
 import Constants
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
 cap = cv2.VideoCapture(Constants.CAPTURE_SOURCE_ID)
 cap.set (3,640)
 cap.set(4,480)
 
 while (True):
     ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
