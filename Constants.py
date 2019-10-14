c53acd50d7b6c301578f1c56195d3b098fc8b545
Benny Mirisola <bennymirisola@gmail.com>
2019-10-14 13:15:10 -0400

    updated scale_factor

--- Constants.py
+++ Constants.py
@@ -2,5 +2,5 @@ CAPTURE_SOURCE_ID = 0
 HAARCASCADE_LOCATION = '/home/pi/python_projects/OpenCV/data/haarcascades/haarcascade_frontalface_default.xml'
 
 SCALE_FACTOR = 1.2
-MINIMUM_NEIGHBORS = 6
+MINIMUM_NEIGHBORS = 10
 MINIMUM_SIZE = (30,30)
