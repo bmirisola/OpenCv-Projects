8461b7479914d61682ba7518cb151f9c1555e24d
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 13:25:10 -0400

    changed camera resolution to lower

--- Constants.py
+++ Constants.py
@@ -1,6 +1,6 @@
 CAPTURE_SOURCE_ID = 0
-CAMERA_WIDTH = 320
-CAMERA_HEIGHT = 240
+CAMERA_WIDTH = 160
+CAMERA_HEIGHT = 120
 
 MODEL = "paths/res10_300x300_ssd_iter_140000.caffemodel"
 #IMAGE = "images/Ha-mil.jpg"
