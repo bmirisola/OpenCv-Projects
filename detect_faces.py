f99c4f5b65ca0e3e33fa7138d0658f4f3e12069c
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 13:39:33 -0400

    commented out graphics

--- detect_faces.py
+++ detect_faces.py
@@ -53,11 +53,10 @@ while True:
         y = startY - 10 if startY - 10 > 10 else startY + 10
         cv2.rectangle(frame, (startX, startY), (endX, endY),
                       (0, 0, 255), 2)
-        cv2.putText(frame, text, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        #cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
     # show the output frame
-    cv2.imshow("Frame", frame)
+    #cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
