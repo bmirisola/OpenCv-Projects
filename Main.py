f812d2f7a95bbb2dec23b522436a3c91ec4695fd
Benny Mirisola <bennymirisola@gmail.com>
2019-10-14 23:48:36 -0400

    added camera width and height constants

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ import numpy as np
 import Constants
 
 cap = cv2.VideoCapture(Constants.CAPTURE_SOURCE_ID)
-cap.set (3,640)
-cap.set(4,480)
+cap.set (3,Constants.CAMERA_WIDTH)
+cap.set(4,Constants.CAMERA_HEIGHT)
 
 while (True):
     ret, frame = cap.read()
