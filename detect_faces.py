b4fd0221bc2a9a24707fa50ed9e8f071df44876f
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 01:31:22 -0400

    copied tutorial verbatim

--- detect_faces.py
+++ detect_faces.py
@@ -13,7 +13,7 @@ print("[INFO] starting video stream...")
 vs = VideoStream(src=0).start()
 #For pi camera use:
 #VideoStream(usePiCamera=True).start()
-time.sleep(2.0)
+time.sleep(2)
 
 # load the input image and construct an input blob for the image
 # by resizing to a fixed 300x300 pixels and then normalizing it
@@ -42,27 +42,30 @@ while True:
 
         # filter out weak detections by ensuring the `confidence` is
         # greater than the minimum confidence
-        if confidence > Constants.CONFIDENCE:
-            # compute the (x, y)-coordinates of the bounding box for the
-            # object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        if confidence < Constants.CONFIDENCE:
+            continue
 
-            # draw the bounding box of the face along with the associated
-            # probability
-            text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(frame, (startX, startY), (endX, endY),
-                          (0, 0, 255), 2)
-            cv2.putText(frame, text, (startX, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        # compute the (x, y)-coordinates of the bounding box for the
+        # object
+        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        (startX, startY, endX, endY) = box.astype("int")
 
-    # show the output image
-    cv2.imshow("Frame", frame)
-    cv2.waitKey(0)
+        # draw the bounding box of the face along with the associated
+        # probability
+        text = "{:.2f}%".format(confidence * 100)
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY),
+                      (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        # show the output frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-    key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
 
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    vs.stop()
