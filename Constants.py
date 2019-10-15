52c8e4757be3192b71ea9109cfb54072edfbc97b
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 00:40:12 -0400

    updated proto and model paths to relative paths

--- Constants.py
+++ Constants.py
@@ -2,7 +2,7 @@ CAPTURE_SOURCE_ID = 0
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
 
-MODEL = "paths\res10_300x300_ssd_iter_140000.caffemodel"
+MODEL = "paths/res10_300x300_ssd_iter_140000.caffemodel"
 IMAGE = "images/Ha-mil.jpg"
-PROTOTEXT = "paths\deploy.prototxt.txt"
+PROTOTEXT = "paths/deploy.prototxt.txt"
 CONFIDENCE = 0.5
