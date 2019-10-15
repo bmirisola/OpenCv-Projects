2681f2f4794b5d5c9f44d0fa5272f893ed1a8d2b
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 00:51:30 -0400

    reverted threshold

--- Constants.py
+++ Constants.py
@@ -6,4 +6,4 @@ MODEL = "paths/res10_300x300_ssd_iter_140000.caffemodel"
 IMAGE = "images/Ha-mil.jpg"
 #IMAGE = "images/Benny and Link.jpg"
 PROTOTEXT = "paths/deploy.prototxt.txt"
-CONFIDENCE = 0.1
+CONFIDENCE = 0.5
