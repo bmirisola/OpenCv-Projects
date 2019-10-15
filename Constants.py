ffc57f52a1c0873bb0f23bd50cc99fe3860d55a5
Benny Mirisola <bennymirisola@gmail.com>
2019-10-15 00:21:57 -0400

    added paths to algorithm

--- Constants.py
+++ Constants.py
@@ -2,7 +2,7 @@ CAPTURE_SOURCE_ID = 0
 CAMERA_WIDTH = 640
 CAMERA_HEIGHT = 480
 
-MODEL = ""
+MODEL = "paths\res10_300x300_ssd_iter_140000.caffemodel"
 IMAGE = ""
-PROTOTEXT = ""
+PROTOTEXT = "paths\deploy.prototxt.txt"
 CONFIDENCE = 0.5
--- Main.py
+++ Main.py
@@ -1,15 +1,25 @@
 import cv2
 import Constants
 
-cap = cv2.VideoCapture(Constants.CAPTURE_SOURCE_ID)
-cap.set (3,Constants.CAMERA_WIDTH)
-cap.set(4,Constants.CAMERA_HEIGHT)
+# cap = cv2.VideoCapture(Constants.CAPTURE_SOURCE_ID)
+# cap.set (3,Constants.CAMERA_WIDTH)
+# cap.set(4,Constants.CAMERA_HEIGHT)
+#
+# while (True):
+#     ret, frame = cap.read()
+#     cv2.imshow('frame', frame)
+#
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
 
-while (True):
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(Constants.PROTOTEXT, Constants.MODEL)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+image = cv2.imread(args["image"])
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0,
+                             (300, 300), (104.0, 177.0, 123.0))
